e7c7c6dada36505900b58f636635b0dab4f377b9
Iuri Dantas <iuri.d2003@aluno.ifsc.edu.br>
2024-04-07 11:52:39 -0300

    comit push

--- libprg/src/libprg/lista_contato.c
+++ libprg/src/libprg/lista_contato.c
@@ -49,6 +49,7 @@ void buscar_contato(lista_c * lista , char * nome){
             printf("Nome : %s ", lista->contatos[i].nome);
             printf("Email : %s ", lista->contatos[i].email);
             printf("Telefone : %s ", lista->contatos[i].telefone);
+            printf("---Contato Encotrado---");
             return;
         }
     }
