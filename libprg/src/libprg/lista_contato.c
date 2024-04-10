f7622021c81cb352cae8166c3480ed55fa97eff9
Iuri <iuri.d2003@aluno.ifsc.edu.br>
2024-04-10 11:02:53 -0300

    Commit

--- libprg/src/libprg/lista_contato.c
+++ libprg/src/libprg/lista_contato.c
@@ -44,7 +44,7 @@ int buscar_contato(lista_c  * lista, char nome[100]) {
 void imprimir_contatos(lista_c *lista) {
     printf("--LISTA DE CONTATOS--\n");
     for (int i = 0; i < lista->tamanho; ++i) {
-        printf("CONTATO [%d] | ",i + 1);
+        printf("CONTATO [%d] | \n ",i + 1);
         printf(" Nome : %s | Email : %s | Telefone : %s | ", lista->contatos[i].nome,
                lista->contatos[i].email , lista->contatos[i].telefone);
     }
