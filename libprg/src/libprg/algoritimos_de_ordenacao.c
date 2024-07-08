4d080ff8aad30efff9ff0451e017881e1b1196a7
Iuri Dantas <iuri.d2003@aluno.ifsc.edu.br>
2024-07-08 14:07:39 -0300

    Insertion , bublle  e selection

--- libprg/src/libprg/algoritimos_de_ordenacao.c
+++ libprg/src/libprg/algoritimos_de_ordenacao.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int burble_sort(int vetor[], int tamanho){
+void burble_sort(int vetor[], int tamanho){
     int aux = 0 ;
     int i , j ;
     for (int i = 0; i < tamanho ;++i) {
@@ -16,4 +16,75 @@ int burble_sort(int vetor[], int tamanho){
     }
 }
 
+int *insertion_sort(int *vetor, int tamanho){
+    int chave = 0 ;
+    int i , j = 0 ;
+    for ( i = 1; i < tamanho ; i++) {
+        chave = vetor[i];
+        j = i - 1  ;
+        while (j >= 0 && vetor[j] > chave){
+            vetor[j + 1] = vetor[j] ;
+            j = j - 1 ;
 
+        }
+        vetor[j + 1] = chave ;
+    }
+    return vetor ;
+}
+
+void selection_sort(int *vet, int tamanho) {
+    int min = 0;
+    int aux = 0;
+    for (int i = 0; i < tamanho ; i++) {
+        min = i;
+        for (int j = i + 1; j < tamanho ; ++j) {
+            if (vet[j] < vet[min]) {
+                min = j;
+            }
+        }
+        if (i != min) {
+            aux = vet[i];
+            vet[i] = vet[min];
+            vet[min] = aux;
+        }
+    }
+}
+int main(void){
+
+    srand(time(NULL));
+   int vet[5] = {} ;
+
+
+    for (int i = 0 ; i < 5; ++i) {
+        vet[i] = rand() % 100 ;
+    }
+    printf("Vetor : ");
+    for (int i = 0; i < 5 ; ++i) {
+        printf("%d,",vet[i]);
+    }printf("\n");
+
+//    burble_sort(vet,5);
+//    printf("Vetor Ordenado : ");
+//
+//    for (int i = 5; i > -1 ; --i) {
+//         printf("%d,",vet[i]);
+//    }
+//
+
+//     insertion_sort(vet,5);
+//     printf("Vetor Ordenada : ");
+//    for (int i = 0; i < 5; i++) {
+//        printf("%d, ",vet[i]);
+//    }
+//    printf("\n");
+
+    selection_sort(vet,5);
+    printf("Vetor ordenando : ");
+
+    for (int i = 0; i < 5; ++i) {
+        printf("%d, ",vet[i]);
+    }
+
+
+    return 0 ;
+}
