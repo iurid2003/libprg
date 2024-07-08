ea8b0752a4efb91d7a7bb5c550d53e7190a4115d
Iuri Dantas <iuri.d2003@aluno.ifsc.edu.br>
2024-07-08 18:01:27 -0300

    decrescente e crescente

--- libprg/src/libprg/algoritimos_de_ordenacao.c
+++ libprg/src/libprg/algoritimos_de_ordenacao.c
@@ -16,6 +16,18 @@ void burble_sort(int vetor[], int tamanho){
     }
 }
 
+void decrescente(int *vetor , int tamanho){
+    for (int i = 0; i < 5; ++i) {
+        printf("%d, ",vetor[i]);
+    }
+}
+
+void crescente(int *vetor, int tamanho){
+    for (int i = tamanho; tamanho > -1 ; --i) {
+        printf("%d, ",vetor[i]);
+    }
+}
+
 int *insertion_sort(int *vetor, int tamanho){
     int chave = 0 ;
     int i , j = 0 ;
