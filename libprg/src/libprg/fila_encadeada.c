0135ac455678b285b0a1910288a955de4e381622
Iuri Dantas <iuri.d2003@aluno.ifsc.edu.br>
2024-07-14 13:47:03 -0300

    Pilha encadeada

--- libprg/src/libprg/fila_encadeada.c
+++ libprg/src/libprg/fila_encadeada.c
@@ -1,3 +1,42 @@
-//
-// Created by aluno on 22/04/24.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//inserir na fila
+
+typedef struct no_f{
+    int valor ;
+     struct  no_f * proximo ;
+}No_f;
+
+
+void inserir_fila(No_f**fila, int num){
+    No_f * novo , *aux = malloc(sizeof(No_f));
+    if (novo){
+        novo->valor = num ;
+        novo->proximo = NULL;
+        if (*fila == NULL){
+            *fila = novo ;
+        }else{
+             aux = *fila ;
+            while (aux->proximo){
+                aux = aux->proximo ;
+                aux->proximo = novo ;
+            }
+        }
+    }else{
+        printf("Erro ao alocar memoria");
+    }
+}
+
+No_f * remover_da_fila(No_f ** fila){
+    No_f  * remover = NULL;
+    if(*fila){
+        remover = *fila ;
+        *fila = remover->proximo ;
+
+    }else{
+        printf("Fila Vazia");
+    }
+    return remover;
+}
