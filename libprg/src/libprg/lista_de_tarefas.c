53b13b208cbac81b62b468186cfd9cd118203fdc
Iuri Dantas <iuri.d2003@aluno.ifsc.edu.br>
2024-07-27 11:15:39 -0300

    Lista_tarefas

--- libprg/src/libprg/lista_de_tarefas.c
+++ libprg/src/libprg/lista_de_tarefas.c
@@ -4,8 +4,8 @@
 #include "libprg/libprg.h"
 
 
-lista_t *lista_tarefa() {
-    lista_t *tarefa= (lista_t *) malloc(sizeof(lista_t ));
+ lista_t *lista_tarefa() {
+     lista_t *tarefa= (lista_t *) malloc(sizeof(lista_t ));
     tarefa->tamanho = 0 ;
     tarefa->capacidade = 50 ;
     tarefa->Tarefas = (Tarefas_t *)calloc(tarefa->capacidade,sizeof(Tarefas_t));
